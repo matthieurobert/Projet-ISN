a164d06597b0d28b9b216a73bbcd0f3e7741b332
Matthieu Robert <matthieurobert82@gmail.com>
2019-03-30 18:33:33 +0100

    update

--- Partie_Clément/salle.py
+++ Partie_Clément/salle.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 def salle():
     from random import randrange
-    from pos_boîte import position_boîte
+    from Partie_Clément.pos_boîte import position_boîte
 
     position_x_y = position_boîte()
     caisses=len (position_x_y)
@@ -13,13 +13,13 @@ def salle():
     fenetre = pygame.display.set_mode((1145,720))
 
 
-    table = pygame.image.load("images/salle.png").convert()
+    table = pygame.image.load("Partie_Clément/images/salle.png").convert()
     fenetre.blit(table, (0,0))
 
-    perso = pygame.image.load("images/perso.jpg").convert()                         # !!! à voir ...
-    fenetre.blit(perso, (30,30))
+    #perso = pygame.image.load("images/perso.jpg").convert()                         # !!! à voir ...
+    #fenetre.blit(perso, (30,30))
 
-    caisse = pygame.image.load("images/boîte.jpg").convert()
+    caisse = pygame.image.load("Partie_Clément/images/boîte.jpg").convert()
     pygame.display.flip()
 
     for i in range (0,caisses):
@@ -30,9 +30,12 @@ def salle():
 
     pygame.display.flip()
 
+    liste = [fenetre, table, caisse, caisses, position_x_y]
+    return liste
 
 
-salle()
+
+
 
 
 
