96d0a96f92482e4e9eb506dd4bcac8b7d637a19c
LaymajouxClement <laymajouxclement@gmail.com>
2019-04-18 13:03:17 +0200

    ajout des com

--- Partie_Clément/nombre_obstacle.py
+++ Partie_Clément/nombre_obstacle.py
@@ -1,12 +1,13 @@
 def nombre_caisse():
-    from random import randrange
-
+    from random import randrange # on créer une fonction qui donne un nombre
+                                 # entre 5 et 14 ( c'est le nombre de caisses )
     nombre = randrange(5,15)
 
     return (nombre)
 
-def nombre_caisse_spe():
-    from random import randrange
+def nombre_caisse_spe(): # On verra ça plus tard si on rajoute
+
+    from random import randrange    # des caisses spéciales
 
     nombre = randrange(1,4)
 
